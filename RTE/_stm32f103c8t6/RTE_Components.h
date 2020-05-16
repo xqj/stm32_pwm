ce6780ac1a50db071ef6908cf85fa20e5d2caf12
xqj <devop@xieqj.net>
2020-05-16 19:27:05 +0800

    GPIO
    基础测试

--- RTE/_stm32f103c8t6/RTE_Components.h
+++ RTE/_stm32f103c8t6/RTE_Components.h
@@ -22,12 +22,8 @@
 #define RTE_DEVICE_STDPERIPH_GPIO
 /*  Keil::Device:StdPeriph Drivers:RCC:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_RCC
-/*  Keil::Device:StdPeriph Drivers:SPI:3.5.0 */
-#define RTE_DEVICE_STDPERIPH_SPI
 /*  Keil::Device:StdPeriph Drivers:TIM:3.5.0 */
 #define RTE_DEVICE_STDPERIPH_TIM
-/*  Keil::Device:StdPeriph Drivers:USART:3.5.0 */
-#define RTE_DEVICE_STDPERIPH_USART
 
 
 #endif /* RTE_COMPONENTS_H */
--- main.c
+++ main.c
@@ -1,11 +1,27 @@
 #include "stm32f10x.h"
 
-
-int main(void)
+void delay_nus(unsigned long n)
 {
+	unsigned long j;
+	while(n--)
+	{
+		j=8;
+		while(j--);
+	}
+}
+void delay_nms(unsigned long n)
+{
+	while(n--)
+		delay_nus(1100);
+}
+int main(void)
+{ 
  
     while(1)
     {
-       
+       GPIO_SetBits(GPIOB,GPIO_Pin_1);
+			 delay_nms(500);
+			 GPIO_ResetBits(GPIOB,GPIO_Pin_1);
+			 delay_nms(500);
     }
 }
