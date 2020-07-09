b0108ffa66c0ac6532e7b1f207c9c41bc9634952
xqj <devop@xieqj.net>
2020-07-09 18:27:23 +0800

    通信成功

--- main.c
+++ main.c
@@ -1,98 +1,116 @@
 #include "stm32f10x.h"
+#include "stdio.h"
+#include "stm32f10x_usart.h"
+#include "stm32f10x.h"
+#include "stm32f10x_iwdg.h"
 
-#define DATA_LEN 1
-#define BOUND 9600
+u8 USART1_RX_BUF[1];
+u8 USART1_RX_CNT=0;
 
-char Usart_Data_Buffer;
-int count=0;
-
-void delay_ms(u16 time)
+void Usart1_Init(u32 bound)
 {
-    u16 i=0;
-    while(time--)
-    {
-        i=12000;
-        while(i--);
-    }
+    //GPIO???úéè??
+    GPIO_InitTypeDef GPIO_InitStructure;
+    USART_InitTypeDef USART_InitStructure;
+    NVIC_InitTypeDef NVIC_InitStructure;
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1|RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOB, ENABLE);//ê1?üUSART1,GPIOA,Cê±?ó
+
+    //USART1_TX   GPIOA.9
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; //PA.9
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;    //?′ó?í?íìê?3?
+    GPIO_Init(GPIOA, &GPIO_InitStructure);//3?ê??ˉGPIOA.9
+
+    //USART1_RX      GPIOA.103?ê??ˉ
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;//PA10
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//????ê?è?
+    GPIO_Init(GPIOA, &GPIO_InitStructure);//3?ê??ˉGPIOA.10
+	
+		GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0; //PA.9
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;    //?′ó?í?íìê?3?
+    GPIO_Init(GPIOB, &GPIO_InitStructure);//3?ê??ˉGPIOA.9
+
+    //Usart1 NVIC ????
+    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);    //éè??NVIC?D??·?×é2:2???à??ó??è??￡?2???ìó|ó??è??   0-3;
+
+    NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3 ;//?à??ó??è??3
+    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;        //×óó??è??3
+    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;            //IRQí¨μàê1?ü
+    NVIC_Init(&NVIC_InitStructure);    //?ù?Y???¨μ?2?êy3?ê??ˉVIC??′??÷
+
+   //USART 3?ê??ˉéè??
+
+    USART_InitStructure.USART_BaudRate = bound;//′??ú2¨ì??ê
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;//×?3¤?a8??êy?Y??ê?
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;//ò???í￡?1??
+    USART_InitStructure.USART_Parity = USART_Parity_No;//?T????D￡?é??
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;//?Tó2?têy?Yá÷????
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;    //ê?·￠?￡ê?
+
+    USART_Init(USART1, &USART_InitStructure); //3?ê??ˉ′??ú1
+    USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//?a??′??ú?óêü?D??
+    USART_Cmd(USART1, ENABLE);                    //ê1?ü′??ú1
 }
-//USART1的中断
-void USART1_IRQHandler(void)
+/**
+* USART1·￠?ílen??×??ú.
+* buf:·￠?í??ê×μ??·
+* len:·￠?íμ?×??úêy(?aá?oí±?′ú??μ??óê??￥??,?aà??¨òé2?òa3?1y64??×??ú)
+**/
+void USART1_Send_Data(u8 *buf,u16 len)
 {
+    u16 t;
    
-    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+//  RS485_TX_EN=1;            //éè???a·￠?í?￡ê?
+    for(t=0;t<len;t++)        //?-?··￠?íêy?Y
     {
-        Usart_Data_Buffer =USART_ReceiveData(USART1);//读取收到数据
+        while(USART_GetFlagStatus(USART1,USART_FLAG_TC)==RESET); //?-?··￠?í,?±μ?·￠?ííê±?
+        USART_SendData(USART1,buf[t]);
     }
-		//溢出-发生溢出需要先读SR，再读DR寄存器则可清除不断入中断的问题
+    while(USART_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);  
+//    RS485_TX_EN=0;                //éè???a?óê??￡ê?
+}
+
+/**
+* ?óê????¨3¤?èμ?×?·?′?
+* ±èè??óê?1ì?¨′óD??a21??×??úμ?×?·?′?
+**/
+void USART1_IRQHandler(void)                    //′??ú1?D??·t??3ìDò
+{
+	
+    u8 Res;
+    if(USART_GetITStatus(USART1, USART_IT_RXNE) != RESET)
+        {
+            Res =USART_ReceiveData(USART1);    //?áè??óê?μ?μ?êy?Y
+            if(USART1_RX_CNT<1)//??óú?óê????¨3¤?èμ?×?·?′?
+            {
+                USART1_RX_BUF[USART1_RX_CNT]=Res;        //?????óê?μ?μ??μ
+                USART1_RX_CNT++;                                        //?óê?êy?Y???ó1
+            }
+     }
+         //ò?3?-è?1?·￠éúò?3?Dèòa?è?áSR,?ù?áDR??′??÷?ò?é??3y2???è??D??μ??êìa
     if(USART_GetFlagStatus(USART1,USART_FLAG_ORE) == SET)
     {
         USART_ReceiveData(USART1);
         USART_ClearFlag(USART1,USART_FLAG_ORE);
     }
-    USART_ClearFlag(USART1,USART_IT_RXNE);//清除接收中断
-		count++;
+     USART_ClearFlag(USART1,USART_IT_RXNE); //ò??¨òa??3y?óê??D??
 }
-/*
-蓝牙通信
-pwm控制
-马达速度控制
-*/
-int main()
+int main(void)
 {
-
-    /*--------------------------------------GPIO-----------------------------------------------*/
-    GPIO_InitTypeDef GPIO_InitStrue;
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA|RCC_APB2Periph_GPIOB,ENABLE);//GPIOA端口使能,对应APB2
-    //TX对应蓝牙RX
-    GPIO_InitStrue.GPIO_Mode=GPIO_Mode_AF_PP;
-    GPIO_InitStrue.GPIO_Pin=GPIO_Pin_9;
-    GPIO_InitStrue.GPIO_Speed=GPIO_Speed_10MHz;
-    GPIO_Init(GPIOA,&GPIO_InitStrue);
-    //RX对应蓝牙TX
-    GPIO_InitStrue.GPIO_Mode=GPIO_Mode_IN_FLOATING;
-    GPIO_InitStrue.GPIO_Pin=GPIO_Pin_10;
-    GPIO_InitStrue.GPIO_Speed=GPIO_Speed_10MHz;
-    GPIO_Init(GPIOA,&GPIO_InitStrue);
-
-    GPIO_InitStrue.GPIO_Pin = GPIO_Pin_0;
-    GPIO_InitStrue.GPIO_Mode = GPIO_Mode_Out_PP;
-    GPIO_InitStrue.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOB, &GPIO_InitStrue);
-    /*--------------------------------------USART-----------------------------------------------*/
-    //嵌套向量中断控制器组选择
-    NVIC_InitTypeDef NVIC_InitStrue;
-    NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
-    NVIC_InitStrue.NVIC_IRQChannel=USART2_IRQn;
-    //使能中断
-    NVIC_InitStrue.NVIC_IRQChannelCmd=ENABLE;
-    //抢占优先级别1
-    NVIC_InitStrue.NVIC_IRQChannelPreemptionPriority=1;
-    //子优先级别1
-    NVIC_InitStrue.NVIC_IRQChannelSubPriority=1;
-    //初始化配置NVIC
-    NVIC_Init(&NVIC_InitStrue);
-		RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-    //蓝牙
-    USART_InitTypeDef USART_InitStrue;
-    USART_InitStrue.USART_BaudRate=BOUND;//串口波特率
-    USART_InitStrue.USART_HardwareFlowControl=USART_HardwareFlowControl_None;//无硬件数据流控制
-    USART_InitStrue.USART_Mode=USART_Mode_Tx|USART_Mode_Rx;//收发模式
-    USART_InitStrue.USART_Parity=USART_Parity_No;//无奇偶校验位
-    USART_InitStrue.USART_StopBits=USART_StopBits_1;//一个停止位
-    USART_InitStrue.USART_WordLength=USART_WordLength_8b;//字长为8位数据格式
-
-    USART_Init(USART1,&USART_InitStrue);//初始化串口1
-    USART_ITConfig(USART1,USART_IT_RXNE,ENABLE);//开启接收中断
-    USART_Cmd(USART1,ENABLE);//使能串口1
-
+    Usart1_Init(9600);//′??ú12¨ì??êéè???a9600  
     while(1)
-    {
-			if(Usart_Data_Buffer=='1'){
-				 GPIO_SetBits(GPIOB,GPIO_Pin_0);
-			}
-			if(Usart_Data_Buffer=='2'){
-				 GPIO_ResetBits(GPIOB,GPIO_Pin_0);
-			}
+    {       
+        if(USART1_RX_CNT==1)//êy?Y?óê?íê3é
+        {
+					 GPIO_ResetBits(GPIOB,GPIO_Pin_0);
+            USART1_RX_CNT=0;//?????′??
+            //???óê?μ?μ?êy?Y·￠?í3?è￥
+						USART1_RX_BUF[0]='6';
+            USART1_Send_Data(USART1_RX_BUF,1);//í¨1y′??ú1???óê?μ?μ?1ì?¨3¤?è×?·?·￠?í3?è￥
+        }
     }
-}
 
+}
